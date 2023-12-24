9607cdaddb85400945bdc3a17a1502cf24a89cc1
utku99 <56312196+utku99@users.noreply.github.com>
2023-12-24 18:20:53 +0300

    t

--- model/companyOrders.js
+++ model/companyOrders.js
@@ -12,22 +12,29 @@ const companyOrdersSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         required: true,
     },
-    menuId: {
-        type: String,
+    companyId: {
+        type: mongoose.Schema.Types.ObjectId,
         required: true,
     },
-    title: {
-        type: String,
-        required: true,
-    },
-    content: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: String,
-        required: true,
-    },
+    orders: [{
+        id: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+    }]
+
 })
 
 export const companyOrdersModel = mongoose.model(" companyorders", companyOrdersSchema)
